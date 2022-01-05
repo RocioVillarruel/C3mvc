b2360cce2abb85415ac674737ff3104d445f1132
RocioVillarruel <87556862+RocioVillarruel@users.noreply.github.com>
2022-01-05 11:07:42 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using C3mvc.Models;
+
+namespace C3mvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly List<Team> _teams;
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+        _teams = new List<Team>();
+    }
+
+    public IActionResult Teams()
+    {
+        _teams.Add(new Team("Barcelona", 1));
+        _teams.Add(new Team("Atletico", 2));
+        _teams.Add(new Team("Real Madrid", 3));
+        return View(_teams);
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/Team.cs
@@ -0,0 +1,13 @@
+namespace C3mvc.Models;
+class Team
+{
+    public string name { get; set; }
+    public int id { get; set; }
+
+public Team(string name, int id)
+{
+    this.name = name;
+    this.id=id;
+}
+
+}
